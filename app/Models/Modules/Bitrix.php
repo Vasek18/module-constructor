832041ad352c05c6e7b0de69198e251814b3abc2
Vasek18 <aristov-92@mail.ru>
2015-12-25 15:36:28 +0300

    При создании модуля на Битрикс, не заполненные поля в таблицы пользователей сохраняются в бд

--- app/Models/Modules/Bitrix.php
+++ app/Models/Modules/Bitrix.php
@@ -4,6 +4,8 @@ namespace App\Models\Modules;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Auth;
+use App\Models\User;
 
 class Bitrix extends Model{
 	//
@@ -25,6 +27,7 @@ class Bitrix extends Model{
 		// запись в БД
 		// todo валидация данных
 		// todo user_id
+		// todo наверное надо проверять на уникальность пару код партнёра / код модуля
 		$bitrix->MODULE_NAME = $request->MODULE_NAME;
 		$bitrix->MODULE_DESCRIPTION = $request->MODULE_DESCRIPTION;
 		$bitrix->MODULE_CODE = $request->MODULE_CODE;
@@ -32,5 +35,19 @@ class Bitrix extends Model{
 		$bitrix->PARTNER_URI = $request->PARTNER_URI;
 		$bitrix->PARTNER_CODE = $request->PARTNER_CODE;
 		$bitrix->save();
+
+		// на будущее сохраняем какие-то поля в таблицу пользователя, если они не указаны
+		$user = User::find(Auth::user()->id); // имхо id можно и меньше кровью получить
+		if (!$user->bitrix_partner_code){
+			$user->bitrix_partner_code = $request->PARTNER_CODE;
+		}
+		if (!$user->bitrix_company_name){
+			$user->bitrix_company_name = $request->PARTNER_NAME;
+		}
+		if (!$user->site){
+			$user->site = $request->PARTNER_URI;
+		}
+		$user->save();
+
 	}
 }
