9e576ee0bcd6594f9d1be1b6da232b0ab16b89c5
Vasek18 <aristov-92@mail.ru>
2016-02-04 00:59:29 +0300

    Немножко переводов

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Пароли должны быть не менее шести символов в длину и соответствовать условию проверки.',
+    'reset' => 'Ваш пароль был сброшен!',
+    'sent' => 'Мы выслали вам по электронной почте ссылку для сброса вашего пароля!',
+    'token' => 'Этот токен сброса пароля недействителен.',
+    'user' => "Мы не можем найти пользователя с таким адресом электронной почты.",
+
+];
