10038b8c9c54d518233bf7ae61463abbf80c4c00
Vasek18 <aristov-92@mail.ru>
2016-02-08 02:33:24 +0300

    С окна доп. настроек сохранятся значения

--- public/js/bitrix_module_admin_options.js
+++ public/js/bitrix_module_admin_options.js
@@ -1,3 +1,21 @@
+// показ модального окна с доп. настройками
+// todo вывод уже установленных значений
+$('#admin_options_dop_settings_window').on('show.bs.modal', function(event){
+	var button = $(event.relatedTarget);
+	var row = button.parents(".row.option");
+	var type_id = row.find("[name $= _type]").val();
+	var modal_form = $('#admin_options_dop_settings_window form');
+	var option_number = row.find('[name=option_number]').val();
+
+	// запоминаем номер строки
+	modal_form.find('[name=option_number]').val(option_number);
+
+	// скрываем все поля
+	modal_form.find('.form-group').hide();
+
+	// показываем поля доступные для этого типа поля
+	modal_form.find('[data-for_type_ids *= "' + type_id + '"]').show();
+});
 $('#admin_options_dop_settings_window').on('show.bs.modal', function(event){
 	var button = $(event.relatedTarget);
 	var row = button.parents(".row.option");
@@ -8,4 +26,22 @@ $('#admin_options_dop_settings_window').on('show.bs.modal', function(event){
 
 	// показываем поля доступные для этого типа поля
 	$("#admin_options_dop_settings_window").find('[data-for_type_ids *= "' + type_id + '"]').show();
+});
+
+// сохранение значений полей
+$('#admin_options_dop_settings_window .save').on('click', function(event){
+	var form = $('#admin_options_dop_settings_window form');
+	var option_number = form.find('[name=option_number]').val();
+	var row = $(".row.option:has([name=option_number][value=" + option_number + "])");
+
+	// сохраняем поля на странице
+	var height = form.find("[name=option_height]").val();
+	row.find("[name$=_height]").val(height);
+	var width = form.find("[name=option_width]").val();
+	row.find("[name$=_width]").val(width);
+
+	$("#admin_options_dop_settings_window").modal("hide");
+	form.trigger("reset");
+
+	return false;
 });
--- resources/views/bitrix/admin_options_dop_settings_window.blade.php
+++ resources/views/bitrix/admin_options_dop_settings_window.blade.php
@@ -1,34 +1,38 @@
 <div class="modal fade" tabindex="-1" role="dialog" id="admin_options_dop_settings_window">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                            aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Дополнительные настройки</h4>
-            </div>
-            <div class="modal-body">
-                <div class="form-group" data-for_type_ids="2">
-                    <label for="">Высота</label>
-                    <input class="form-control" type="text" name="" id="" value="">
+            <form>
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Дополнительные настройки</h4>
                 </div>
-                <div class="form-group" data-for_type_ids="1,2">
-                    <label for="">Ширина</label>
-                    <input class="form-control" type="text" name="" id="" value="">
-                </div>
-                <div class="form-group" data-for_type_ids="3,4,5">
-                    <label for="">Значения</label>
-                    <input class="form-control" type="text" name="" id="" value="">
-                </div>
-                <div class="form-group" data-for_type_ids="3,4,5">
-                    <label for="">Специальные значения</label>
-                    <div class="special-vals-list">
+                <div class="modal-body">
+                    <input type="hidden" name="option_number">
+                    <div class="form-group" data-for_type_ids="2">
+                        <label for="option_height">Высота</label>
+                        <input class="form-control" type="text" name="option_height" id="option_height" value="">
+                    </div>
+                    <div class="form-group" data-for_type_ids="1,2">
+                        <label for="option_width">Ширина</label>
+                        <input class="form-control" type="text" name="option_width" id="option_width" value="">
+                    </div>
+                    <div class="form-group" data-for_type_ids="3,4,5">
+                        <label for="option_{{$i}}_vals">Значения</label>
+                        <input class="form-control" type="text" name="option_{{$i}}_vals" id="option_{{$i}}_vals"
+                               value="">
+                    </div>
+                    <div class="form-group" data-for_type_ids="3,4,5">
+                        <label for="">Специальные значения</label>
+                        <div class="special-vals-list">
 
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Закрыть</button>
-            </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary save">Закрыть</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
