fb9b33ed393d06d0e3353ee936509679e30ccee2
Vasek18 <aristov-92@mail.ru>
2016-02-09 01:33:02 +0300

    Миграция для обработчиков событий

---
+++ database/migrations/2016_02_09_012322_create_bitrix_events_handlers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBitrixEventsHandlersTable extends Migration{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up(){
+		Schema::create('bitrix_events_handlers', function (Blueprint $table){
+			$table->increments('id');
+			$table->integer('module_id');
+			$table->string('event');
+			$table->string('class');
+			$table->string('handler');
+			$table->longText('php_code')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down(){
+		Schema::drop('bitrix_events_handlers');
+	}
+}
