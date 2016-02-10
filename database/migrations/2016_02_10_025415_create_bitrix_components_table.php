3f7a900a98cafe3a68ae09e2c17ea058d5086183
Vasek18 <aristov-92@mail.ru>
2016-02-10 03:00:30 +0300

    Миграция для основной таблицы компонентов

---
+++ database/migrations/2016_02_10_025415_create_bitrix_components_table.php
@@ -0,0 +1,32 @@
+<?php
+
+// таблица для хранения компонентов
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBitrixComponentsTable extends Migration{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up(){
+		Schema::create('bitrix_components', function (Blueprint $table){
+			$table->increments('id');
+			$table->integer('module_id');
+			$table->string('name');
+			$table->text('desc');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down(){
+		Schema::drop('bitrix_components');
+	}
+}
