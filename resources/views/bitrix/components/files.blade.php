dd4dd7f20628d0694b9d9589a4be9208b1a3c09b
Vasek18 <aristov-92@mail.ru>
2016-04-17 17:20:45 +0300

    Поработал со вьюшками компонента

---
+++ resources/views/bitrix/components/files.blade.php
@@ -0,0 +1,39 @@
+<div id="files">
+    <h2>Файлы</h2>
+    <div class="files">
+       <table class="table">
+           <tr>
+               <th></th>
+               <th>lang</th>
+           </tr>
+           <tr>
+               <th></th>
+               <th>ru</th>
+           </tr>
+           <tr>
+               <td>
+                   <a href="#">component.php</a>
+               </td>
+               <td>
+                   <a href="#">component.php</a>
+               </td>
+           </tr>
+           <tr>
+               <td>
+                   <a href="#">.description.php</a>
+               </td>
+               <td>
+                   <a href="#">.description.php</a>
+               </td>
+           </tr>
+           <tr>
+               <td>
+                   <a href="#">.parameters.php</a>
+               </td>
+               <td>
+                   <a href="#">.parameters.php</a>
+               </td>
+           </tr>
+       </table>
+    </div>
+</div>
