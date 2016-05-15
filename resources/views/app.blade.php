90295ad558a3f955060eaa03e7c1eb633bd79e71
Vasek18 <aristov-92@mail.ru>
2016-05-15 15:23:58 +0300

    Подстилил главную

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
     <title>Конструктор модулей</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/app.css">
+    <link href='https://fonts.googleapis.com/css?family=Lato:100,400,700,900' rel='stylesheet'>
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -30,11 +31,19 @@
                 <div class="collapse navbar-collapse" id="top-navbar-collapse-1">
                     <ul class="nav navbar-nav navbar-right">
                         @if (!Auth::check())
-                            <li><a href="{{ route('auth') }}">Авторизация</a></li>
-                            <li><a href="{{ route('reg') }}">Регистрация</a></li>
+                            <li>
+                                <a href="{{ route('auth') }}">Авторизация</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('reg') }}">Регистрация</a>
+                            </li>
                         @else
-                            <li><a href="{{ route('personal') }}">Личный кабинет</a></li>
-                            <li><a href="{{ route('logout') }}">Выйти</a></li>
+                            <li>
+                                <a href="{{ route('personal') }}">Личный кабинет</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('logout') }}">Выйти</a>
+                            </li>
                         @endif
                     </ul>
                 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,22 +3,31 @@
 @section("content")
     <div class="container">
         <div class="row">
-            <div class="banner-on-main col-md-8 col-md-offset-2">
-                <h2>Делаешь сайты, а зарабатываешь меньше 100 000р в месяц?</h2>
-                <hr>
+            <div class="banner-on-main clearfix">
+                <div class="col-md-8">
+                    <strong>Делаешь сайты, а зарабатываешь меньше 100 000р в месяц?</strong>
+                </div>
             </div>
-            <div class="banner-on-main col-md-8 col-md-offset-2">
-                <h2>Не можешь создать модуль под Битрикс, потому что это слишком сложно?</h2>
-                <hr>
+            <div class="banner-on-main clearfix">
+                <div class="col-md-8 col-md-offset-4">
+                    <strong>Не можешь создать модуль под Битрикс, потому что это слишком сложно?</strong>
+                </div>
             </div>
-            <div class="banner-on-main col-md-8 col-md-offset-2">
-                <h2>Часто повторяешь свой код?</h2>
-                <hr>
+            <div class="banner-on-main clearfix">
+                <div class="col-md-8">
+                    <strong>Часто повторяешь свой код?</strong>
+                </div>
             </div>
-            <div class="banner-on-main col-md-8 col-md-offset-2">
-                <h2>Хватит это терпеть!</h2>
-                <p>Не, серьёзно, это же совсем не сложно теперь. Просто заполняй простые формочки в сервисе, читай
-                    подсказки и на выходе получишь готовый для публикации в Marketplace архив с модулем.</p>
+            <div class="banner-on-main clearfix">
+                <div class="col-md-8 col-md-offset-4">
+                    <strong>Хватит это терпеть!</strong>
+                    <p>Не, серьёзно, это же совсем не сложно теперь. Просто заполняй простые формочки в сервисе,
+                        читай
+                        подсказки и на выходе получишь готовый для публикации в Marketplace архив с модулем.
+                        <br>
+                    И просто качай бабло.
+                    </p>
+                </div>
             </div>
             <div class="col-md-8 col-md-offset-2 modules-created">
                 <p>С нами создано уже</p>
