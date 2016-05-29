4d166395b7ccb4c2c4920646c397278a2493c775
Vasek18 <aristov-92@mail.ru>
2016-05-29 17:02:45 +0300

    createMailEvent и createMailTemplate

---
+++ app/helpers/helpers.php
@@ -0,0 +1,74 @@
+<?php
+function upgradeVersionNumber($version){
+	$versionArr = explode(".", $version);
+	$lastIndex = count($versionArr) - 1;
+	$versionArr[$lastIndex]++;
+	$version = implode(".", $versionArr);
+
+	return $version;
+}
+
+// todo это должно быть по идее в пространстве имён Битрикса
+function extractLangVal($val, $langfile){
+	preg_match('/GetMessage\([\"\']+([^\"\']+)/is', $val, $matches);
+	if (isset($matches[1])){
+		$vArrParse = new App\Helpers\vArrParse;
+		$langVals = $vArrParse->parseFromFile($langfile, '$MESS');
+		//dd($langVals);
+
+		if (isset($langVals[$matches[1]])){
+			//dd($langVals[$matches[1]]);
+			return $langVals[$matches[1]];
+		}else{
+			return '';
+		}
+	}
+
+	return $val;
+}
+
+function ifStringIsValName($string){
+	preg_match('/^\$([a-zA-Z_]+)/is', trim($string), $matches);
+	if ($matches[0]){
+		return true;
+	}
+
+	return false;
+}
+
+// todo это должно быть по идее в пространстве имён Битрикса
+function getLangFilesForThisFile($root, $file){
+	// todo захардкожен диск
+	$answer = [];
+
+	$dirs = \Storage::disk('user_modules_bitrix')->directories($root.'/lang');
+	foreach ($dirs as $dir){
+		$langFiles = \Storage::disk('user_modules_bitrix')->files($dir);
+		foreach ($langFiles as $langFile){
+			if ($dir.$file == $langFile){
+				$dirPathArr = explode('/', $dir);
+				$langKey = $dirPathArr[count($dirPathArr) - 1];
+
+				$langFile = str_replace($root, '', $langFile);
+				$answer[$langKey] = $langFile;
+			}
+		}
+	}
+
+	return $answer;
+}
+
+function getFunctionTextForSearchInText($text, $functionName){
+	$function = false;
+
+	$beginning = strpos($text, 'function '.$functionName); // todo вариант, если поставлено болле одного пробела
+	if ($beginning === false){
+		return false;
+	}else{
+		return $beginning;
+	}
+
+	return $function;
+}
+
+?>
--- storage/modules_templates/bitrix/install/index.php
+++ storage/modules_templates/bitrix/install/index.php
@@ -209,6 +209,38 @@ Class {MODULE_CLASS_NAME} extends CModule{
 		return $ibp->Add($arFieldsProp);
 	}
 
+	function createMailEvent($code, $name, $description, $sort, $LID = 'ru'){
+		$params = Array(
+			"LID"         => $LID,
+			"EVENT_NAME"  => $code,
+			"NAME"        => $name,
+			"SORT"        => $sort,
+			"DESCRIPTION" => $description
+		);
+		$rsET = CEventType::GetList(Array("TYPE_ID" => $code, "LID" => $LID));
+		if ($arET = $rsET->Fetch()){
+			$event = CEventType::Update(array("ID" => $arET["ID"]), $params);
+			$eventID = $arET["ID"];
+		}else{
+			$event = new CEventType;
+			$eventID = $event->Add($params);
+		}
+
+		return $eventID;
+	}
+
+	function createMailTemplate($params){
+		$params["ACTIVE"] = "Y";
+		if (!isset($params["LID"])){
+			$params["LID"] = SITE_ID;
+		}
+
+		$template = new CEventMessage;
+		$templateID = $template->Add($params);
+
+		return $templateID;
+	}
+
 	public function isVersionD7(){
 		return CheckVersion(\Bitrix\Main\ModuleManager::getVersion('main'), '14.00.00');
 	}
