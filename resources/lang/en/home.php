7587cdd59a63cec8d579a98ecbfb1a18d12703e9
Vasek18 <aristov-92@mail.ru>
2016-06-05 14:08:36 +0300

    Перевёл главную

---
+++ resources/lang/en/home.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'little_earnings'    => 'Do you making websites but earn less than 5k per month?',
+	'bitrix_difficult'   => 'Do you can not create a module for Bitrix, because it is too difficult?',
+	'code_repeat'        => 'Do you often repeat your code?',
+	'stop_it'            => 'Enough!',
+	'why_stop_it'        => 'No, seriously, it\'s not so difficult today. Simply fill simple forms in the service, read tips and you\'d get ready for publication in the Marketplace module archive. <br> And simply get the money.',
+	'there_were_created' => 'With us were created',
+	'modules'             => 'module|modules',
+];
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,14 +21,14 @@
             <div class="banner-on-main clearfix">
                 <div class="col-md-8 col-md-offset-4">
                     <strong>{{trans('home.stop_it')}}</strong>
-                    <p>{{trans('home.why_stop_it')}}
+                    <p>{!! trans('home.why_stop_it') !!}
                     </p>
                 </div>
             </div>
             <div class="col-md-8 col-md-offset-2 modules-created">
                 <p>{{trans('home.there_were_created')}}</p>
                 <strong>{{$countModules}}</strong>
-                <p>{{trans('home.modules', ['ru_ending' => $modulesEnding])}}</p>
+                <p>{{trans_choice('home.modules', $countModules, ['ru_ending' => $modulesEnding])}}</p>
             </div>
         </div>
     </div>
