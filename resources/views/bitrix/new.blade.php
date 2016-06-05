65a88d4794cff5eff4b03e7d675e8330487fd611
Vasek18 <aristov-92@mail.ru>
2016-06-05 16:14:45 +0300

    Перевёл надписи на странице создания модуля Битрикса

--- resources/views/bitrix/new.blade.php
+++ resources/views/bitrix/new.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Создание модуля для Битрикс - шаг 1</div>
+                    <div class="panel-heading">{{trans('bitrix_create.form_title')}}</div>
                     <div class="panel-body">
                         @if (count($errors) > 0)
                             <div class="alert alert-danger">
@@ -21,81 +21,84 @@
                               action="{{ action('Modules\Bitrix\BitrixController@store') }}">
                             {{ csrf_field() }}
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Название модуля</label>
+                                <label class="col-md-4 control-label">{{trans('bitrix_create.module_name_label')}}</label>
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="MODULE_NAME"
                                            value="{{ old('MODULE_NAME') }}" required aria-describedby="MODULE_NAME_help"
                                            id="module_name">
-                                    <span class="help-block" id="MODULE_NAME_help">Под этим названием модуль будет показываться на сайте Маркетплейса, а также в админке у покупателей, в том числе и в списке модулей с настройками</span>
+                                    <span class="help-block"
+                                          id="MODULE_NAME_help">{!!trans('bitrix_create.module_name_help')!!}</span>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Описание модуля</label>
+                                <label class="col-md-4 control-label">{{trans('bitrix_create.module_desc_label')}}</label>
                                 <div class="col-md-6">
                                     <textarea name="MODULE_DESCRIPTION" class="form-control"
                                               aria-describedby="MODULE_DESCRIPTION_help">{{ old('MODULE_DESCRIPTION') }}</textarea>
-                                    <span class="help-block" id="MODULE_DESCRIPTION_help">Это описание будет показываться только в админке у покупателей в списках установленных модулей. Не обязательно</span>
+                                    <span class="help-block"
+                                          id="MODULE_DESCRIPTION_help">{!!trans('bitrix_create.module_desc_help')!!}</span>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Код модуля</label>
+                                <label class="col-md-4 control-label">{{trans('bitrix_create.module_code_label')}}</label>
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="MODULE_CODE"
                                            value="{{ old('MODULE_CODE') }}" required pattern="[a-z]+[a-z0-9]*"
                                            aria-describedby="MODULE_CODE_help" data-translit_from="module_name"
                                            id="module_code">
-                                    <span class="help-block" id="MODULE_CODE_help">Идентификатор модуля на сайте Битрикса и в админках покупателей. Учавствует в названии модуля {Код партнёра}.{Код модуля}.<br>Только маленькие латинские буквы</span>
+                                    <span class="help-block"
+                                          id="MODULE_CODE_help">{!!trans('bitrix_create.module_code_help')!!}</span>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Версия модуля</label>
+                                <label class="col-md-4 control-label">{{trans('bitrix_create.module_version_label')}}</label>
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="MODULE_VERSION"
                                            value="0.0.1" required pattern="[0-9\.]+"
                                            aria-describedby="MODULE_VERSION_help">
-                                    <span class="help-block" id="MODULE_VERSION_help">Версия модуля.<br>3 цифры разделённые точками не ниже 0.0.1</span>
+                                    <span class="help-block"
+                                          id="MODULE_VERSION_help">{!!trans('bitrix_create.module_version_help')!!}</span>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Имя партнёра</label>
+                                <label class="col-md-4 control-label">{{trans('bitrix_create.partner_name_label')}}</label>
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="PARTNER_NAME"
                                            value="{{ $user["bitrix_company_name"]?$user["bitrix_company_name"]:$user["company_name"] }}"
                                            required aria-describedby="PARTNER_NAME_help">
-                                    <span class="help-block" id="PARTNER_NAME_help">Ваше имя или название вашей компании. Будет отображаться в авторах модуля</span>
+                                    <span class="help-block"
+                                          id="PARTNER_NAME_help">{!!trans('bitrix_create.partner_name_help')!!}</span>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Ссылка на ваш сайт</label>
+                                <label class="col-md-4 control-label">{{trans('bitrix_create.partner_uri_label')}}</label>
                                 <div class="col-md-6">
                                     <input type="url" class="form-control" name="PARTNER_URI"
                                            required value="{{ $user["site"] }}" aria-describedby="PARTNER_URI_help">
-                                    <span class="help-block" id="PARTNER_URI_help">Ссылка на ваш сайт или сайт вашей компании. Будет отображаться как ссылка на авторов модуля</span>
+                                    <span class="help-block"
+                                          id="PARTNER_URI_help">{!!trans('bitrix_create.partner_uri_help')!!}</span>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Код партнёра</label>
+                                <label class="col-md-4 control-label">{{trans('bitrix_create.partner_code_label')}}</label>
                                 <div class="col-md-6">
                                     <input type="text" class="form-control" name="PARTNER_CODE"
                                            value="{{ $user["bitrix_partner_code"] }}" required pattern="[a-z]+[a-z0-9]*"
                                            aria-describedby="PARTNER_CODE_help">
-                                    <span class="help-block" id="PARTNER_CODE_help">Код партнёра, который указан у вас в личном кабинете партнёра на сайте Битрикса. Учавствует в названии модуля {Код партнёра}.{Код модуля}.<br>Только маленькие латинские буквы</span>
+                                    <span class="help-block"
+                                          id="PARTNER_CODE_help">{!!trans('bitrix_create.partner_code_help')!!}</span>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary" name="module_create">
-                                        Создать модуль
-                                    </button>
+                                    <button type="submit" class="btn btn-primary"
+                                            name="module_create">{{trans('bitrix_create.button')}}</button>
                                 </div>
                             </div>
                         </form>
                         <div class="step-description">
-                            <h2>Описание шага</h2>
-                            <p>На этом шаге создаётся простейший модуль для Битрикса. По сути это всего лишь шаблон
-                                модуля, с заполненными основными полями, поскольку в нём отсутствует какой-либо полезный
-                                функционал. Уже на данном этапе модуль можно устанавливать и удалять; всю полезную
-                                нагрузку можно будет создать в последующих шагах.</p>
+                            <h2>{{trans('bitrix_create.form_desc_title')}}</h2>
+                            <p>{{trans('bitrix_create.form_desc_text')}}</p>
                         </div>
                     </div>
                 </div>
