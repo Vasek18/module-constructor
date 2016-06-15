694c92ea29675a9a562740d7f79ddda598e41a99
Vasek18 <aristov-92@mail.ru>
2016-06-16 00:58:43 +0300

    Ланг для индексной хранения данных

---
+++ resources/lang/en/bitrix_data_storage.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'h1'                => 'Data storage',
+	'iblocks'           => 'Infoblocks',
+	'add_iblock_button' => 'Add infoblock',
+];
--- resources/views/bitrix/data_storage/index.blade.php
+++ resources/views/bitrix/data_storage/index.blade.php
@@ -1,14 +1,13 @@
 @extends('bitrix.internal_template')
 
 @section('h1')
-    Хранение данных
+    {{ trans('bitrix_data_storage.h1') }}
 @stop
 
 @section('page')
 
-    <h2>Инфоблоки</h2>
-    <a href="{{action('Modules\Bitrix\BitrixDataStorageController@add_ib', [$module->id])}}" class="btn btn-primary">Добавить
-        иб</a>
+    <h2>{{ trans('bitrix_data_storage.iblocks') }}</h2>
+    <a href="{{action('Modules\Bitrix\BitrixDataStorageController@add_ib', [$module->id])}}" class="btn btn-primary">{{ trans('bitrix_data_storage.add_iblock_button') }}</a>
     <br>
     <br>
     @if (count($infoblocks))
