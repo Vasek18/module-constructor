2c003d9fcc113db2f10e2be8f8c6f6648414df93
Vasek18 <aristov-92@mail.ru>
2016-08-06 22:06:09 +0300

    Убрал галочки у пунктов меню компонента

--- resources/views/bitrix/components/progress_way_menu.blade.php
+++ resources/views/bitrix/components/progress_way_menu.blade.php
@@ -17,22 +17,22 @@
              id="component_menu">
             <ol class="nav navbar-nav">
                 <li class="{{Route::is('bitrix_component_detail') ? 'active':''}}">
-                    <a href="{{route('bitrix_component_detail', [$module->id, $component->id])}}">{{ trans('bitrix_components.menu_title_main_info') }}{!! in_array(1, $component->steps)?' <span class="badge success"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></span>':''!!}</a>
+                    <a href="{{route('bitrix_component_detail', [$module->id, $component->id])}}">{{ trans('bitrix_components.menu_title_main_info') }}</a>
                 </li>
                 <li class="{{Route::is('bitrix_component_visual_path') ? 'active':''}}">
-                    <a href="{{route('bitrix_component_visual_path', [$module->id, $component->id])}}">{{ trans('bitrix_components.menu_title_visual_path') }}{!! in_array(2, $component->steps)?' <span class="badge success"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></span>':''!!}</a>
+                    <a href="{{route('bitrix_component_visual_path', [$module->id, $component->id])}}">{{ trans('bitrix_components.menu_title_visual_path') }}</a>
                 </li>
                 <li class="{{Route::is('bitrix_component_params') ? 'active':''}}">
-                    <a href="{{route('bitrix_component_params', [$module->id, $component->id])}}">{{ trans('bitrix_components.menu_title_params') }}{!! in_array(3, $component->steps)?' <span class="badge success"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></span>':''!!}</a>
+                    <a href="{{route('bitrix_component_params', [$module->id, $component->id])}}">{{ trans('bitrix_components.menu_title_params') }}</a>
                 </li>
                 <li class="{{Route::is('bitrix_component_component_php') ? 'active':''}}">
-                    <a href="{{route('bitrix_component_component_php', [$module->id, $component->id])}}">{{ trans('bitrix_components.menu_title_component_php') }}{!! in_array(4, $component->steps)?' <span class="badge success"><span class="glyphicon glyphicon-ok aria-hidden="true"></span></span>':''!!}</a>
+                    <a href="{{route('bitrix_component_component_php', [$module->id, $component->id])}}">{{ trans('bitrix_components.menu_title_component_php') }}</a>
                 </li>
                 <li class="{{Route::is('bitrix_component_other_files') ? 'active':''}}">
-                    <a href="{{route('bitrix_component_other_files', [$module->id, $component->id])}}">{{ trans('bitrix_components.menu_title_other_files') }}{!! in_array(5, $component->steps)?' <span class="badge success"><span class="glyphicon glyphicon-ok aria-hidden="true"></span></span>':''!!}</a>
+                    <a href="{{route('bitrix_component_other_files', [$module->id, $component->id])}}">{{ trans('bitrix_components.menu_title_other_files') }}</a>
                 </li>
                 <li class="{!! classActiveSegment(5, 'templates') !!}">
-                    <a href="{{route('bitrix_component_templates', [$module->id, $component->id])}}">{{ trans('bitrix_components.menu_title_templates') }}{!! in_array(6, $component->steps)?' <span class="badge success"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></span>':''!!}</a>
+                    <a href="{{route('bitrix_component_templates', [$module->id, $component->id])}}">{{ trans('bitrix_components.menu_title_templates') }}</a>
                 </li>
             </ol>
         </div>
