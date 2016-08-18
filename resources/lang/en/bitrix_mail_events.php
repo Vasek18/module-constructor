7176b48bdb471722bd501e5519c0b342430ba73f
Vasek18 <aristov-92@mail.ru>
2016-08-18 10:59:16 +0300

    Перевёл индексную почтовых событий

---
+++ resources/lang/en/bitrix_mail_events.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'h1'          => 'Mail events',
+	'button_add'  => 'Add mail event',
+	'mail_events' => 'Mail events',
+];
--- resources/views/bitrix/mail_events/index.blade.php
+++ resources/views/bitrix/mail_events/index.blade.php
@@ -1,24 +1,26 @@
 @extends('bitrix.internal_template')
 
 @section('h1')
-    Почтовые события
+    {{ trans('bitrix_mail_events.h1') }}
 @stop
 
 @section('page')
-
-    <a href="{{action('Modules\Bitrix\BitrixMailEventsController@create', [$module->id])}}" class="btn btn-primary">Добавить почтовое событий</a>
+    <a href="{{action('Modules\Bitrix\BitrixMailEventsController@create', [$module->id])}}"
+       class="btn btn-primary">{{ trans('bitrix_mail_events.button_add') }}</a>
     <hr>
     @if (count($mail_events))
-        <h2>Почтовые события</h2>
+        <h2>{{ trans('bitrix_mail_events.mail_events') }}</h2>
         <div class="list-group">
             @foreach($mail_events as $mail_event)
-                <div class="list-group-item clearfix component">
+                <div class="list-group-item clearfix mail_event deletion_wrapper">
                     <a href="{{ action('Modules\Bitrix\BitrixMailEventsController@show', [$module->id, $mail_event->id]) }}">
                         "{{$mail_event->name}}" ({{$mail_event->code}})
                     </a>
                     <a href="{{ action('Modules\Bitrix\BitrixMailEventsController@destroy', [$module->id, $mail_event->id]) }}"
-                       class="btn btn-danger pull-right">
-                        <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                       class="btn btn-danger pull-right human_ajax_deletion"
+                       id="delete_mail_event_{{$mail_event->id}}">
+                    <span class="glyphicon glyphicon-trash"
+                          aria-hidden="true"></span>
                     </a>
                 </div>
             @endforeach
