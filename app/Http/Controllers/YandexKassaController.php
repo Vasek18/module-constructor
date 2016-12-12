ec6b265bfc03fa9672a1205279017fda74233dda
Vasek18 <aristov-92@mail.ru>
2016-12-13 01:49:19 +0300

    Работа над оплатой

--- app/Http/Controllers/YandexKassaController.php
+++ app/Http/Controllers/YandexKassaController.php
@@ -2,20 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Request;
+use Illuminate\Support\Facades\Log;
+
 class YandexKassaController extends Controller{
-	public function checkOrder(){
-		return 'checkOrder';
+
+	public function checkOrder(Request $request){
+		Log::info('YK checkOrder ', $request->all());
+
+		$code = $this->isValidHash($request);
+
+		$response = $this->generateResponseContent($request, $code);
+
+		return response($response, 200, [
+			'Content-type' => 'application/xml'
+		]);
 	}
 
-	public function paymentAviso(){
+	public function paymentAviso(Request $request){
+		Log::info('YK paymentAviso ', $request->all());
+
 		return 'paymentAviso';
 	}
 
-	public function success(){
+	public function success(Request $request){
+		Log::info('YK success ', $request->all());
+
 		return 'success';
 	}
 
-	public function fail(){
+	public function fail(Request $request){
+		Log::info('YK fail ', $request->all());
+
 		return 'fail';
+	}
+
+	public function isValidHash(Request $request){
+		$hash = md5(htmlspecialchars($request->action).';'.htmlspecialchars($request->orderSumAmount).';'.htmlspecialchars($request->orderSumCurrencyPaycash).';'.htmlspecialchars($request->orderSumBankPaycash).';'.env('YANDEX_KASSA_SHOP_ID').';'.htmlspecialchars($request->invoiceId).';'.htmlspecialchars($request->customerNumber).';'.env('YANDEX_KASSA_SHOP_PASSWORD'));
+		if (strtolower($hash) != strtolower($request->md5)){
+			return 1;
+		}else{
+			return 0;
+		}
+	}
+
+	public function generateResponseContent(Request $request, $code){
+		return '<?xml version="1.0" encoding="UTF-8"?>
+		<checkOrderResponse performedDatetime="'.htmlspecialchars($request->requestDatetime).'" code="'.$code.'"'.' invoiceId="'.htmlspecialchars($request->invoiceId).'" shopId="'.env('YANDEX_KASSA_SHOP_ID').'"/>';
 	}
 }
--- resources/views/personal/oplata_form.blade.php
+++ resources/views/personal/oplata_form.blade.php
@@ -1,12 +1,15 @@
 <div class="row">
     <div class="col-md-4">
-        <form action="">
+        <form action="https://money.yandex.ru/eshop.xml" method="post">
+            <input type="hidden" name="shopId" value="{{ env('YANDEX_KASSA_SHOP_ID') }}">
+            <input type="hidden" name="scid" value="{{ env('YANDEX_KASSA_SCID') }}">
+            <input name="customerNumber" value="{{ $user->id }}" type="hidden">
             <div class="form-group">
                 <label for="sum">{{ trans('oplata_form.sum') }}, {{ trans('oplata_form.rubles') }}</label>
                 <select id="sum"
                         name="sum"
                         class="form-control input-lg">
-                    <option value="{{ convertCurrency(setting('day_price')*1) }}">{{ trans('oplata_form.one_day') }}
+                    <option value="{{ setting('day_price')*1 }}">{{ trans('oplata_form.one_day') }}
                         - {{ convertCurrency(setting('day_price')*1) }}</option>
                     <option value="{{ setting('day_price')*3 }}">{{ trans('oplata_form.three_days') }}
                         - {{ convertCurrency(setting('day_price')*3) }}</option>
