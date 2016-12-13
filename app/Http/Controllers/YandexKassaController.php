329303ba221fa2fed560232d2d52ced9fc7e2951
Vasek18 <aristov-92@mail.ru>
2016-12-14 00:42:41 +0300

    Работа над подключением оплаты

--- app/Http/Controllers/YandexKassaController.php
+++ app/Http/Controllers/YandexKassaController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class YandexKassaController extends Controller{
