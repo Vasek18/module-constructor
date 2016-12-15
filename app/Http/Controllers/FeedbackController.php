6af862b8a57aef537063bd2343f8815f5958c7d6
Vasek18 <aristov-92@mail.ru>
2016-12-16 01:14:32 +0300

    GOD_EMAIL

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -8,11 +8,9 @@ use App\Http\Requests;
 use Mail;
 
 class FeedbackController extends Controller{
-	public $emailto = 'aristov-92@mail.ru';
-
 	public function sendILackSmthForm(Request $request){
 		Mail::send('emails.ilack', ['page' => $request->page, 'email' => $request->email, 'text' => $request->text], function ($m){
-			$m->to($this->emailto)->subject('Кому-то то-то не нравится');
+			$m->to(env('GOD_EMAIL'))->subject('Кому-то то-то не нравится');
 		});
 
 		return back();
@@ -20,7 +18,7 @@ class FeedbackController extends Controller{
 
 	public function sendBugReportForm(Request $request){
 		Mail::send('emails.bug_report', ['page' => $request->page, 'text' => $request->text], function ($m){
-			$m->to($this->emailto)->subject('Сообщение об ошибке');
+			$m->to(env('GOD_EMAIL'))->subject('Сообщение об ошибке');
 		});
 
 		return back();
