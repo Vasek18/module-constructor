d0e9ee9fd8f5ca38574ccdcf0fce7a91feff6bed
Vasek18 <aristov-92@mail.ru>
2017-01-08 14:07:19 +0300

    Вёрстка поплыла

--- resources/views/personal/index.blade.php
+++ resources/views/personal/index.blade.php
@@ -1,14 +1,14 @@
 @extends("app")
 
 @section("content")
-    <h1>{{ Auth::user()->last_name }} {{  Auth::user()->first_name }}
-        @if (setting('day_price'))
-            <small>({{ trans('personal_index.paid_days') }}: {{  Auth::user()->paid_days }})</small>
-        @endif
-    </h1>
-    <hr>
     <div class="container">
         @include('personal.header')
+        <h1>{{ Auth::user()->last_name }} {{  Auth::user()->first_name }}
+            @if (setting('day_price'))
+                <small>({{ trans('personal_index.paid_days') }}: {{  Auth::user()->paid_days }})</small>
+            @endifk
+        </h1>
+        <hr>
         <h2>{{trans('personal_index.modules_list')}}</h2>
         @include('personal.bitrix_block')
     </div>
