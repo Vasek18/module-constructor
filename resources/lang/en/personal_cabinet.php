45979b6d2b4712145a7ede500dd86084296eb7b3
Vasek18 <aristov-92@mail.ru>
2017-01-14 23:28:07 +0300

    Накидал тексты

--- resources/lang/en/personal_cabinet.php
+++ resources/lang/en/personal_cabinet.php
@@ -9,4 +9,7 @@ return [
 	'oplata_h1'       => 'Payment',
 	'help_project_h1' => 'Project help',
 	'donate_h1'       => 'Express gratitude to the project',
+	'donate_p'        => 'Мы сильно вам помогли: вы заработали деньги на модулях или сэкономили кучу своего времени, а соответственно и денег с помощью сервиса и теперь хотите выразить нам благодарность.<br>
+            Сервис помогает всем бесплатно, поэтому лучшей благодарностью будет оплатить разработчикам пиццу и кофе.', // todo
+	'donate_p_after'        => 'Also, simply by using the service more often and talking about it to friends and colleagues, you are helping us nice.',
 ];
--- resources/views/personal/donate.blade.php
+++ resources/views/personal/donate.blade.php
@@ -4,6 +4,8 @@
     <div class="container">
         @include('personal.header')
         <h1>{{ trans('personal_cabinet.donate_h1') }}</h1>
+        <p class="lead">{!! trans('personal_cabinet.donate_p') !!}</p>
         @include('donate_form')
+        <p class="lead">{!! trans('personal_cabinet.donate_p_after') !!}</p>
     </div>
 @stop
