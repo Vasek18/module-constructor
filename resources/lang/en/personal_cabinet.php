01d930bc9b88098c743b22bcfd03c7e8a8ed7bda
Vasek18 <aristov-92@mail.ru>
2017-01-15 16:32:24 +0300

    Перевод

--- resources/lang/en/personal_cabinet.php
+++ resources/lang/en/personal_cabinet.php
@@ -9,7 +9,7 @@ return [
 	'oplata_h1'       => 'Payment',
 	'help_project_h1' => 'Project help',
 	'donate_h1'       => 'Express gratitude to the project',
-	'donate_p'        => 'Мы сильно вам помогли: вы заработали деньги на модулях или сэкономили кучу своего времени, а соответственно и денег с помощью сервиса и теперь хотите выразить нам благодарность.<br>
-            Сервис помогает всем бесплатно, поэтому лучшей благодарностью будет оплатить разработчикам пиццу и кофе.', // todo
+	'donate_p'        => 'We greatly help you: you\'ve earned money for the modules or saved a lot of of your time, and therefore the money through the service and now want to express our gratitude.<br>
+Service helps everyone for free, so it will be grateful to pay developers to pizza and coffee.',
 	'donate_p_after'        => 'Also, simply by using the service more often and talking about it to friends and colleagues, you are helping us nice.',
 ];
