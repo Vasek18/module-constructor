569922a0091baab789827c82a1ac87c3595b3d4b
Vasek18 <aristov-92@mail.ru>
2017-02-02 13:33:48 +0300

    Емеил у контактов

--- resources/views/html_pages/contacts.blade.php
+++ resources/views/html_pages/contacts.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <h1>{{ trans('contacts.h1') }}</h1>
-        <p class="big-text">{{ trans('contacts.my_email') }} <b>admin@modules-constructor.ru</b></p>
+        <p class="big-text">{{ trans('contacts.my_email') }} <b>{{ env('PUBLIC_EMAIL') }}</b></p>
         <p class="big-text">{!! trans('contacts.use_forms') !!}</p>
     </div>
 @stop
