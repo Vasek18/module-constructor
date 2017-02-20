4c252fa80501d2e97760ff93258e389b67971059
Vasek18 <aristov-92@mail.ru>
2017-02-20 14:12:46 +0300

    Подтверждение прав для вебмастера яндекса

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,8 @@
           content="@yield('keywords')">
     <meta name="description"
           content="@yield('description', trans('app.meta_description'))">
+    <meta name="yandex-verification"
+          content="8efd6448266108e3"/>
     <link rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet"
