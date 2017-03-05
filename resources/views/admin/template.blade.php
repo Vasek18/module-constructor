c9064e6a51c37c8e6cd566f57c72ca24603d8571
Vasek18 <aristov-92@mail.ru>
2017-03-05 14:27:35 +0300

    Немного скомпоновал меню в админке

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -16,30 +16,60 @@
                     <a href="{{ action('Admin\AdminController@modules') }}"
                        class="list-group-item">Модули
                     </a>
-                    <a href="{{ action('Admin\AdminController@articles') }}"
-                       class="list-group-item">Статьи
-                    </a>
-                    <a href="{{ action('Admin\AdminConfirmsController@index') }}"
-                       class="list-group-item">На утверждение
-                    </a>
                     <a href="{{ action('Admin\AdminPaymentsController@index') }}"
                        class="list-group-item">Оплаты
                     </a>
-                    <a href="#"
-                       class="list-group-item">Рейтинг функционала
-                    </a>
-                    <a href="{{ action('Admin\AdminLogsController@index') }}"
-                       class="list-group-item">Логи
-                    </a>
                     <a href="{{ action('Admin\AdminSettingsController@index') }}"
-                       class="list-group-item">Основные настройки
+                       class="list-group-item">Основные настройки сервиса
                     </a>
-                    <a href="{{ action('Admin\AdminUserReportsController@index') }}"
-                       class="list-group-item">Репорты
+                    <a class="list-group-item"
+                       role="button"
+                       data-toggle="collapse"
+                       href="#bitrixSubMenu"
+                       aria-expanded="false"
+                       aria-controls="bitrixSubMenu">
+                        Для модулей Битрикса
                     </a>
-                    <a href="{{ action('Admin\AdminProjectPulseController@index') }}"
-                       class="list-group-item">Пульс проекта
+                    <div class="collapse"
+                         id="bitrixSubMenu">
+                        <a href="{{ action('Admin\AdminConfirmsController@index') }}"
+                           class="list-group-item">На утверждение
+                        </a>
+                    </div>
+                    <a class="list-group-item"
+                       role="button"
+                       data-toggle="collapse"
+                       href="#publicTexts"
+                       aria-expanded="false"
+                       aria-controls="publicTexts">
+                        Статьи и новости
                     </a>
+                    <div class="collapse"
+                         id="publicTexts">
+                        <a href="{{ action('Admin\AdminController@articles') }}"
+                           class="list-group-item">Статьи
+                        </a>
+                        <a href="{{ action('Admin\AdminProjectPulseController@index') }}"
+                           class="list-group-item">Пульс проекта
+                        </a>
+                    </div>
+                    <a class="list-group-item"
+                       role="button"
+                       data-toggle="collapse"
+                       href="#serviceImprovement"
+                       aria-expanded="false"
+                       aria-controls="serviceImprovement">
+                        Работа с ошибками сервиса
+                    </a>
+                    <div class="collapse"
+                         id="serviceImprovement">
+                        <a href="{{ action('Admin\AdminLogsController@index') }}"
+                           class="list-group-item">Логи
+                        </a>
+                        <a href="{{ action('Admin\AdminUserReportsController@index') }}"
+                           class="list-group-item">Репорты
+                        </a>
+                    </div>
                 </div>
             </div>
             <div class="col-md-9">
