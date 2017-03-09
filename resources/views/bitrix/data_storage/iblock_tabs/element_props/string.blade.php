a2e970521810403961746f886f1001bf3f81d9ac
Vasek18 <aristov-92@mail.ru>
2017-03-10 01:01:26 +0300

    Вёрстка множественной строки

--- resources/views/bitrix/data_storage/iblock_tabs/element_props/string.blade.php
+++ resources/views/bitrix/data_storage/iblock_tabs/element_props/string.blade.php
@@ -7,5 +7,32 @@
             {{ $property->is_required ? 'required' : '' }}
     >
 @else
-    {{--todo--}}
+    @if (isset($props_vals[$property->id]))
+        @foreach($props_vals[$property->id] as $val)
+            <input type="text"
+                   id="{{$property->id}}"
+                   name="props[{{$property->id}}][]"
+                   class="form-control"
+                   value="{{ $val }}"
+            >
+            <br>
+        @endforeach
+        @for($i=0; $i < 5 - count($props_vals[$property->id]); $i++)
+            <input type="text"
+                   id="{{$property->id}}"
+                   name="props[{{$property->id}}][]"
+                   class="form-control"
+            >
+            <br>
+        @endfor
+    @else
+        @for($i=0; $i < 5; $i++)
+            <input type="text"
+                   id="{{$property->id}}"
+                   name="props[{{$property->id}}][]"
+                   class="form-control"
+            >
+            <br>
+        @endfor
+    @endif
 @endif
