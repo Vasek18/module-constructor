da30027cba13f3179984cb13f43ec7e639c6b143
Vasek18 <aristov-92@mail.ru>
2017-04-10 09:10:13 +0300

    webshim для сафари

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,6 +25,15 @@
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+    <script type="text/javascript"
+            src="https://cdnjs.cloudflare.com/ajax/libs/webshim/1.16.0/dev/polyfiller.js"></script>
+    <script>
+        webshims.activeLang('ru');
+        webshims.polyfill('forms');
+        if (webshims.cfg){
+            webshims.cfg.no$Switch = true;
+        }
+    </script>
     <meta property="og:title"
           content="{{ trans('app.meta_title') }}"/>
     <meta property="og:description"
