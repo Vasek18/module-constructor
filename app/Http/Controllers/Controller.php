2d2473f8c8e2d28ab26569404d700723c8a6326a
Vasek18 <aristov-92@mail.ru>
2017-05-06 20:08:40 +0300

    Перенёс middleware в группу web

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use App\Models\ArticleSection;
-use Auth;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -14,13 +14,19 @@ abstract class Controller extends BaseController{
 	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
 	protected $user;
+	protected $signedIn;
 
 	public function __construct(){
+		$this->middleware(function ($request, $next){
+			$this->user = Auth::user();
+			$this->signedIn = Auth::check();
 
-		$this->user = Auth::user();
+			view()->share('signedIn', $this->signedIn);
+			view()->share('user', $this->user);
 
-		view()->share('signedIn', Auth::check());
-		view()->share('user', $this->user);
+			return $next($request);
+		});
+
 		if (Article::useCasesArticles()){
 			view()->share('use_cases_articles', Article::useCasesArticles()->active()->orderBy('sort')); // статьи о примерах использования
 		}
