c1290beb982aa5fdfc1aa4b755c2a746274dad8f
Vasek18 <aristov-92@mail.ru>
2017-05-14 15:53:39 +0300

    Дальше пытаюсь настроить пасспорт

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
-use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Passport\Passport;
 
@@ -20,11 +19,10 @@ class AuthServiceProvider extends ServiceProvider{
 	/**
 	 * Register any application authentication / authorization services.
 	 *
-	 * @param  \Illuminate\Contracts\Auth\Access\Gate $gate
 	 * @return void
 	 */
-	public function boot(GateContract $gate){
-		parent::registerPolicies($gate);
+	public function boot(){
+		$this->registerPolicies();
 
 		Passport::routes();
 		Passport::tokensExpireIn(Carbon::now()->addYears(20));//You can also use addDays(10)
