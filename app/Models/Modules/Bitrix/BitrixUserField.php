6dbce59fe49b1529ca79f69e85d99b84a8444c8d
Vasek18 <aristov-92@mail.ru>
2017-09-01 21:49:34 +0300

    Модель и миграция для пользовательских полей

---
+++ app/Models/Modules/Bitrix/BitrixUserField.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models\Modules\Bitrix;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BitrixUserField extends Model{
+	protected $table = 'bitrix_user_fields';
+	protected $fillable = [
+		'id',
+		'user_type_id',
+		'entity_id',
+		'field_name',
+		'xml_id',
+		'sort',
+		'multiple',
+		'mandatory',
+		'show_filter',
+		'show_in_list',
+		'edit_in_list',
+		'is_searchable',
+		'settings',
+		'edit_form_label',
+		'list_column_label',
+		'list_filter_label',
+		'error_message',
+		'help_message',
+	];
+	public $timestamps = false;
+	protected $casts = [
+		'settings'          => 'array',
+		'edit_form_label'   => 'array',
+		'list_column_label' => 'array',
+		'list_filter_label' => 'array',
+		'error_message'     => 'array',
+	];
+}
---
+++ database/migrations/2017_09_01_212631_create_bitrix_user_fields_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBitrixUserFieldsTable extends Migration{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up(){
+		Schema::create('bitrix_user_fields', function (Blueprint $table){
+			$table->increments('id');
+			$table->integer('user_type_id');
+			$table->string('entity_id');
+			$table->string('field_name');
+			$table->string('xml_id');
+			$table->string('sort');
+			$table->boolean('multiple');
+			$table->boolean('mandatory');
+			$table->string('show_filter');
+			$table->boolean('show_in_list');
+			$table->boolean('edit_in_list');
+			$table->boolean('is_searchable');
+			$table->text('settings'); // это по идее массивы
+			$table->text('edit_form_label'); // это по идее массивы
+			$table->text('list_column_label'); // это по идее массивы
+			$table->text('list_filter_label'); // это по идее массивы
+			$table->text('error_message'); // это по идее массивы
+			$table->text('help_message'); // это по идее массивы
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down(){
+		Schema::dropIfExists('bitrix_user_fields');
+	}
+}
