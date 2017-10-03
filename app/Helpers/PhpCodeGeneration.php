b9a77af89c147f0c04c419c2ca07ae4822b43188
Vasek18 <aristov-92@mail.ru>
2017-10-04 00:44:46 +0300

    Тест миграции для пользовательского свойства

--- app/Helpers/PhpCodeGeneration.php
+++ app/Helpers/PhpCodeGeneration.php
@@ -9,11 +9,13 @@ class PhpCodeGeneration{
     public static function makeArrayCode($array, $tabsCount = 0, $onEnd = ';'){
         $code = str_repeat("\t", $tabsCount).'Array('.PHP_EOL;
         foreach ($array as $key => $value){
+            $code .= str_repeat("\t", $tabsCount + 1).'"'.$key.'" => ';
+
             if (!is_array($value)){
                 if (static::isString($value)){
-                    $code .= str_repeat("\t", $tabsCount + 1).'"'.$key.'" => "'.$value.'",'.PHP_EOL;
+                    $code .= '"'.$value.'",'.PHP_EOL;
                 } else{ // не обрамляется кавычками
-                    $code .= str_repeat("\t", $tabsCount + 1).'"'.$key.'" => '.$value.','.PHP_EOL;
+                    $code .= $value.','.PHP_EOL;
                 }
             } else{
                 $code .= static::makeArrayCode($value, $tabsCount + 1, ','.PHP_EOL);
--- tests/files/Modules/Bitrix/BitrixUserFieldsFilesTest.php
+++ tests/files/Modules/Bitrix/BitrixUserFieldsFilesTest.php
@@ -8,87 +8,117 @@ use App\Models\Modules\Bitrix\BitrixMailEventsVar;
 /** @group bitrix_files */
 class BitrixUserFieldsFilesTest extends BitrixTestCase{
 
-	use DatabaseTransactions;
+    use DatabaseTransactions;
 
-	protected $path = '/data_storage';
+    protected $path = '/data_storage';
 
-	function setUp(){
-		parent::setUp();
+    function setUp(){
+        parent::setUp();
 
-		$this->signIn();
-		$this->module = $this->fillNewBitrixForm();
-	}
+        $this->signIn();
+        $this->module = $this->fillNewBitrixForm();
+    }
 
-	function tearDown(){
-		parent::tearDown();
+    function tearDown(){
+        parent::tearDown();
 
-		if ($this->module){
-			$this->module->deleteFolder();
-		}
-	}
+        if ($this->module){
+            $this->module->deleteFolder();
+        }
+    }
 
-	function getUserFieldsCreationFuncCallParamsArray($module){
-		$answer = [];
-		$installationFileContent = file_get_contents($module->getFolder(true).'/install/index.php');
-		$gottenInstallationFuncCode = vFuncParse::getFullCode($installationFileContent, 'createNecessaryUserFields');
-		// dd($gottenInstallationFuncCode);
+    function getUserFieldsCreationFuncCallParamsArray($module){
+        $answer                     = [];
+        $installationFileContent    = file_get_contents($module->getFolder(true).'/install/index.php');
+        $gottenInstallationFuncCode = vFuncParse::getFullCode($installationFileContent, 'createNecessaryUserFields');
+        // dd($gottenInstallationFuncCode);
 
-		preg_match_all('/\$this\-\>createUserField\(([^\;]+)\);/is', $gottenInstallationFuncCode, $matches);
-		// dd($matches);
+        preg_match_all('/\$this\-\>createUserField\(([^\;]+)\);/is', $gottenInstallationFuncCode, $matches);
+        // dd($matches);
 
-		foreach ($matches[1] as $gottenInstallationFuncCodePart){
-			$answer[] = explode(', ', $gottenInstallationFuncCodePart);
-		}
+        foreach ($matches[1] as $gottenInstallationFuncCodePart){
+            $answer[] = vArrParse::parseFromText($gottenInstallationFuncCodePart);
+        }
 
-		return $answer;
-	}
+        //        dd($answer);
 
+        return $answer;
+    }
 
-	function getLangFileArray($module, $lang = 'ru'){
-		$optionsFileContent = $this->disk()->get($module->module_folder.'/lang/'.$lang.'/install/index.php');
-		$optionsArr = vArrParse::parseFromText($optionsFileContent, 'MESS');
+    function getLangFileArray($module, $lang = 'ru'){
+        $optionsFileContent = $this->disk()->get($module->module_folder.'/lang/'.$lang.'/install/index.php');
+        $optionsArr         = vArrParse::parseFromText($optionsFileContent, 'MESS');
 
-		return $optionsArr;
-	}
+        return $optionsArr;
+    }
 
-	/** @test */
-	function at_first_there_is_no_optional_functions(){
-		$installationFileContent = file_get_contents($this->module->getFolder(true).'/install/index.php');
+    /** @test */
+    function at_first_there_is_no_optional_functions(){
+        $installationFileContent = file_get_contents($this->module->getFolder(true).'/install/index.php');
 
-		$this->assertFalse(strpos($installationFileContent, 'function createUserField'));
-		$this->assertFalse(strpos($installationFileContent, 'function removeUserField'));
-	}
+        $this->assertFalse(strpos($installationFileContent, 'function createUserField'));
+        $this->assertFalse(strpos($installationFileContent, 'function removeUserField'));
+    }
 
-	/** @test */
-	function it_saves_mailevent_creation_code(){
-		// $user_field = $this->createUserFieldOnForm($this->module, [
-		// 	"entity_id"           => "USER",
-		// 	"field_name"          => "UF_TEST",
-		// 	"edit_form_label[ru]" => "Test"
-		// ]);
-		//
-		// $gottenInstallationFuncCodeArray = $this->getUserFieldsCreationFuncCallParamsArray($this->module);
-		// $installFileLangArr = $this->getLangFileArray($this->module);
-		// $installationFileContent = file_get_contents($this->module->getFolder(true).'/install/index.php');
-		//
-		// $expectedInstallationFuncCodeArray = [
-		// 	'"TEST_MAIL"',
-		// 	'Loc::getMessage("'.$this->module->lang_key.'_MAIL_EVENT_TEST_MAIL_NAME")',
-		// 	'Loc::getMessage("'.$this->module->lang_key.'_MAIL_EVENT_TEST_MAIL_DESC")',
-		// 	'1808',
-		// ];
-		//
-		// $this->assertEquals(1, count($gottenInstallationFuncCodeArray));
-		// $this->assertEquals($expectedInstallationFuncCodeArray, $gottenInstallationFuncCodeArray[0]);
-		//
-		// $this->assertArrayHasKey($this->module->lang_key.'_MAIL_EVENT_TEST_MAIL_NAME', $installFileLangArr);
-		// $this->assertEquals($installFileLangArr[$this->module->lang_key.'_MAIL_EVENT_TEST_MAIL_NAME'], 'TestMail');
-		//
-		//
-		// // проверка, что есть вспомогательные функции
-		// $this->assertNotFalse(strpos($installationFileContent, 'function createUserField'));
-		// $this->assertNotFalse(strpos($installationFileContent, 'function removeUserField'));
-	}
+    /** @test */
+    function it_saves_user_field_creation_code(){
+        $user_field = $this->createUserFieldOnForm($this->module, [
+            "entity_id"           => "USER",
+            "field_name"          => "UF_TEST",
+            "edit_form_label[ru]" => "Test"
+        ]);
+
+        $gottenInstallationFuncCodeArray = $this->getUserFieldsCreationFuncCallParamsArray($this->module);
+        $installFileLangArr              = $this->getLangFileArray($this->module);
+        $installationFileContent         = file_get_contents($this->module->getFolder(true).'/install/index.php');
+
+        $expectedInstallationFuncCodeArray = [
+            "USER_TYPE_ID"      => "string",
+            "ENTITY_ID"         => "USER",
+            "FIELD_NAME"        => "UF_TEST",
+            "XML_ID"            => "",
+            "SORT"              => "100",
+            "MULTIPLE"          => "N",
+            "MANDATORY"         => "N",
+            "SHOW_FILTER"       => "N",
+            "SHOW_IN_LIST"      => "N",
+            "EDIT_IN_LIST"      => "N",
+            "IS_SEARCHABLE"     => "N",
+            "SETTINGS"          => [
+                "DEFAULT_VALUE" => "",
+                "SIZE"          => "20",
+                "ROWS"          => "1",
+                "MIN_LENGTH"    => "0",
+                "MAX_LENGTH"    => "0",
+                "REGEXP"        => "",
+            ],
+            "EDIT_FORM_LABEL"   => [
+                "ru" => 'Loc::getMessage("'.$this->module->lang_key.'_USER_FIELD_UF_TEST_EDIT_FORM_LABEL")',
+            ],
+            "LIST_COLUMN_LABEL" => [
+                "ru" => 'Loc::getMessage("'.$this->module->lang_key.'_USER_FIELD_UF_TEST_LIST_COLUMN_LABEL")',
+            ],
+            "LIST_FILTER_LABEL" => [
+                "ru" => 'Loc::getMessage("'.$this->module->lang_key.'_USER_FIELD_UF_TEST_LIST_FILTER_LABEL")',
+            ],
+            "ERROR_MESSAGE"     => [
+                "ru" => 'Loc::getMessage("'.$this->module->lang_key.'_USER_FIELD_UF_TEST_ERROR_MESSAGE")',
+            ],
+            "HELP_MESSAGE"      => [
+                "ru" => 'Loc::getMessage("'.$this->module->lang_key.'_USER_FIELD_UF_TEST_HELP_MESSAGE")',
+            ],
+        ];
+
+        $this->assertEquals(1, count($gottenInstallationFuncCodeArray));
+        $this->assertEquals($expectedInstallationFuncCodeArray, $gottenInstallationFuncCodeArray[0]);
+
+        $this->assertArrayHasKey($this->module->lang_key.'_USER_FIELD_UF_TEST_EDIT_FORM_LABEL', $installFileLangArr);
+        $this->assertEquals($installFileLangArr[$this->module->lang_key.'_USER_FIELD_UF_TEST_EDIT_FORM_LABEL'], 'Test');
+
+        // проверка, что есть вспомогательные функции
+        $this->assertNotFalse(strpos($installationFileContent, 'function createUserField'));
+        $this->assertNotFalse(strpos($installationFileContent, 'function removeUserField'));
+    }
 
 }
 
