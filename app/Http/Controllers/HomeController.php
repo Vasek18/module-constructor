fdc6f0186c3e11842ef60c03b004737059f719a2
Vasek18 <aristov-92@mail.ru>
2017-11-21 00:47:03 +0300

    Редирект с главной для авторизованных

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,29 +5,38 @@ namespace App\Http\Controllers;
 use App\Models\Metrics\MetricsEventsLog;
 use App\Models\ProjectPulsePost;
 use App\Models\Modules\Bitrix\Bitrix;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller{
 
-	public function index(){
-		$countModules = Bitrix::count();
-		$modulesEnding = 'ей';
-		if (substr($countModules, -1, 1) == '1'){
-			$modulesEnding = 'ь';
-		}
-		if (in_array(substr($countModules, -1, 1), ['2', '3', '4'])){
-			$modulesEnding = 'я';
-		}
-		$data = [
-			'countModules'  => $countModules,
-			'modulesEnding' => $modulesEnding,
-			'project_pulse_posts' => ProjectPulsePost::orderBy('created_at', 'desc')->orderBy('id', 'desc')->take(5)->get()
-		];
+    public function index(){
+        if (Auth::check()){
+            return redirect(action('PersonalController@index'));
+        }
+
+        $countModules  = Bitrix::count();
+        $modulesEnding = 'ей';
+        if (substr($countModules, -1, 1) == '1'){
+            $modulesEnding = 'ь';
+        }
+        if (in_array(substr($countModules, -1, 1), [
+            '2',
+            '3',
+            '4'
+        ])){
+            $modulesEnding = 'я';
+        }
+        $data = [
+            'countModules'        => $countModules,
+            'modulesEnding'       => $modulesEnding,
+            'project_pulse_posts' => ProjectPulsePost::orderBy('created_at', 'desc')->orderBy('id', 'desc')->take(5)->get()
+        ];
 
         // логируем действие
         MetricsEventsLog::log(
             'Открыта главная страница'
         );
 
-		return view("index", $data);
-	}
+        return view("index", $data);
+    }
 }
